dd1a498f2a21de89e776c0b791a111373fe154af
Andrea Mastrantoni <anmastrantoni@gmail.com>
2020-02-19 14:18:27 -0500

    React Performance Optimizations

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,7 @@ import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import "./App.css";
 import { setSearchField, requestRobots } from "../actions";
+import Header from "../components/Header";
 
 const mapStateToProps = state => {
   return {
@@ -35,7 +36,7 @@ class App extends Component {
       <h1>Loading</h1>
     ) : (
       <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
+        <Header />
         <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <CardList robots={filteredRobots} />
