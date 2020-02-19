802f87af78006a5c4b06e41b021a541d5a74ffa7
Andrea Mastrantoni <anmastrantoni@gmail.com>
2020-02-19 09:22:16 -0500

    Redux Async Actions

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware, combineReducers } from "redux";
+import thunkMiddleWare from "redux-thunk";
 import "./index.css";
 import App from "./containers/App";
 import registerServiceWorker from "./registerServiceWorker";
 import "tachyons";
-import { searchRobots } from "./reducers";
+import { searchRobots, requestRobots } from "./reducers";
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
 
 const store = createStore(
-  searchRobots,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  rootReducer,
+  composeEnhancer(applyMiddleware(thunkMiddleWare))
 );
 
 ReactDOM.render(
